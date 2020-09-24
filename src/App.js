c5087aaaeb66f2465ca67b57532dbc5b720da5c8
y2kwak <goodywk07@gmail.com>
2020-09-24 00:15:24 -0400

    mapping data from json file to simulate data fetch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import Card from './Card.js';
 import './App.css';
+import jobs from './jobs.json';
 
 function App() {
+  let content = null
+
+  // if the jobs.json file is successfully imported, map information to job object by the id
+  // this is to potentially use axios.get or to acess data from an API where job details might be outlined
+  if(jobs) {
+    content = jobs.map((job) =>
+      <div key={job.id}>
+        <Card 
+          job={job}
+        />
+      </div>
+    )
+  }
+
+  // else statement could be added to catch failures
+
   return (
     <div className="App">
-        <Card
-          name="Lumaki Labs Inc."
-          position="Software Engineer Intern"
-          description="As a software engineer at Lumaki Labs Inc, you will work with a creative team and a senior engineer to build out the front end…"
-          duration="2-months"
-          level="Intermediate"
-          date="Oct 1, 2020 EDT"
-          logo="../public/LumakiLabs_White_Lg (1).png"
-
-        />
-
-        <Card
-        name="University of Toronto"
-        position="UX/UI Design Intern"
-        description="As a UX/UI Design at the University of Toronto, you will work directly with professors and researchers to find insights into remote..."
-        duration="4-months"
-        level="Beginner"
-        date="Jan. 1, 2020 EDT"
-        logo=""
-        />
- 
+      {content}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -3,38 +3,35 @@ import './Card.css';
 
 function Card(props){
     return (
-        <div>
-        <div className="card">
-            <div className="card-inner">
-            <div
-                style={{
-                    'backgroundImage': `url('${props.logo}')`,
-                }}
-                className="card-img"
-            ></div>
-            <h1>
-                {props.name}
-            </h1>
-            <h2>{props.position}</h2>
-            <p className="description">
-                {props.description}
-            </p>
-            <div className="tag">
-                <p className="tag-title">
-                    Duration: </p>
-                <p id="tag">{props.duration}</p>
+        <div className="spacing">
+            <div className="card">
+              <div className="card-inner">
+                <div
+                    className="card-img"
+                    style={{
+                        'backgroundImage': `url('${props.job.logo}')`,
+                    }}
+                ></div>
+                <h1>{props.job.title}</h1>
+                <h2>{props.job.position}</h2>
+                <p className="description">{props.job.description}</p>
+                <div style={{'marginBottom': '25px'}}>
+                    <div className="grid">
+                        <p className="tag-title">Duration: </p>
+                        <p id="tag">{props.job.duration}</p>
+                    </div>
+                    <div className="grid">
+                        <p className="tag-title">Level:</p>
+                        <p id="tag">{props.job.level}</p>
+                    </div>
+                </div>
+                <p>Apply by: {props.job.date}</p>
+                <div style={{'textAlign': 'right'}}>
+                    <a href={`jobs/${props.job.id}`}><i>Learn more  </i>{'>'}</a>
+                </div>
+              </div>    
             </div>
-            <div className="tag">
-                <p className="tag-title">
-                    Level:</p>
-                <p id="tag">{props.level}</p>
-            </div>
-                <p>Apply by: {props.date}</p>
-            
-            <p style={{textAlign: 'right'}}><i>Learn more</i></p>
-            </div>
-            
-        </div></div>
+        </div>
     )
 }
 export default Card
